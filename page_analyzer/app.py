4797f3ed83d95aa4e769923150ebd4723f2c13e3
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-08-01 20:46:49 +0700

    2 step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def handler():
-    return 'Hello, World!'
+    return render_template("index.html")
